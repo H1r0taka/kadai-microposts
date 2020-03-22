94d860a102248101fca7af59129484ebd68f2286
hirotaka.iwanaga <iwanaga1623@gmail.com>
2020-03-22 14:51:21 +0000

    follow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,11 +25,9 @@ class SessionsController < ApplicationController
   def login(email, password)
     @user = User.find_by(email: email)
     if @user && @user.authenticate(password)
-      
       session[:user_id] = @user.id
       return true
     else
-      
       return false
     end
   end
