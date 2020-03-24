281ce99ac0a08c975b596666492b0982fd0ef6bc
hirotaka.iwanaga <iwanaga1623@gmail.com>
2020-03-24 13:31:46 +0000

    BackUp

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,18 +7,18 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :microposts
-  has_many :relationships
+  has_many :relationships# 
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
 
-  def follow(other_user)
+  def follow(other_user)# フォローするとき
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
     end
   end
 
-  def unfollow(other_user)
+  def unfollow(other_user)# フォローを外すとき
     relationship = self.relationships.find_by(follow_id: other_user.id)
     relationship.destroy if relationship
   end
